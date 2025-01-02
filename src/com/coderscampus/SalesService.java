43aa7649dc896a3181fa36c22676c5cc1178cb5a
Yehoshua Holstein <holsteinyehoshua@gmail.com>
2025-01-02 15:27:32 -0500

    Model3 commit

--- src/com/coderscampus/SalesService.java
+++ src/com/coderscampus/SalesService.java
@@ -1,10 +1,12 @@
 package com.coderscampus;
 
 import java.time.Month;
+import java.time.YearMonth;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class SalesService {
@@ -12,22 +14,28 @@ public class SalesService {
 	public void salesGroupBy(String filePath) {
 	
 		FileService fileService = new FileService();
-		List<Sales> salesYearData = fileService.read(filePath); 
+		List<Sales> salesData = fileService.read(filePath); 
 		
-		Map<Integer, Integer> groupedByYear = salesYearData.stream().collect
+		Map<Integer, Integer> groupedByYear = salesData.stream().collect
 			  (Collectors.groupingBy(Sales::getYear,
 				Collectors.summingInt(Sales::getSalesAmount)));
 		groupedByYear
 			.forEach((key, value) -> System.out.println(key + " -> " + value));
-
+		
         System.out.println("");
+                
+        Map<YearMonth, Integer> monthData = salesData.stream()
+                .collect(Collectors.toMap(Sales::getmonthYear, Sales::getSalesAmount));
         
-        List<Sales> salesMonthData = fileService.read(filePath); 
-        Map<Month, Integer> groupedByMonth = salesMonthData.stream().collect
-  			  (Collectors.groupingBy(Sales::getMonth,
-  				Collectors.summingInt(Sales::getSalesAmount)));
-        Month maxKey = Collections.max(groupedByMonth.entrySet(), Comparator.comparingInt(Map.Entry::getValue)).getKey();
-        Month minKey = Collections.min(groupedByMonth.entrySet(), Comparator.comparingInt(Map.Entry::getValue)).getKey();
+        YearMonth maxKey = monthData.entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .orElse(null);
+        
+        YearMonth minKey = monthData.entrySet().stream()
+                .min(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .orElse(null);
         
         System.out.println("The best month for Model 3 was: " + maxKey);
         System.out.println("The worst month for Model 3 was: " + minKey);
